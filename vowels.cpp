ab6f49c51448dd2ef9d72994c983f059f2d7a553
mahathiunique <mahathiunique@gmail.com>
2024-11-01 13:23:11 +0530

    Create vowels.cpp

---
+++ vowels.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string alphabets;
+    cout << "Type something: ";
+    getline(cin, alphabets); 
+    int vowels = 0;
+
+    for (int i = 0; alphabets[i]; i++) { 
+        if (alphabets[i] == 'a' || alphabets[i] == 'e' || alphabets[i] == 'i' || alphabets[i] == 'o' || alphabets[i] == 'u' ||
+            alphabets[i] == 'A' || alphabets[i] == 'E' || alphabets[i] == 'I' || alphabets[i] == 'O' || alphabets[i] == 'U') {
+            vowels++;
+        }
+    }
+    if (vowels > 0) {
+        cout << "You have " << vowels << " vowels!" << endl;
+    } else {
+        cout << "No vowels are present." << endl;
+    }
+    return 0;
+}
+
+
+
+    
